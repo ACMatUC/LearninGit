e26fca771eff29586ca109c1eb7cad5ea7d856ad
Kurt Lewis <kurt.jlewis15@gmail.com>
2016-08-08 23:06:50 -0400

    Added control for boxes and button to codes.js

--- codes.js
+++ codes.js
@@ -1,3 +1,6 @@
 function Button() {
-	window.alert("Button was pressed");
+	var box1 = document.getElementsByName('Box1')[0].value;
+	var box2 = document.getElementsByName('Box2')[0].value;
+	var out = "The button was pressed!"
+	document.getElementsByName('output')[0].value= out;
 }
