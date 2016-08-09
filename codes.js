9b8b5d0dcb55cb1e50c272d0d75674140db56147
Kurt Lewis <kurt.jlewis15@gmail.com>
2016-08-08 23:13:52 -0400

    Multiply the numbers entered in each box and display it

--- codes.js
+++ codes.js
@@ -1,6 +1,6 @@
 function Button() {
 	var box1 = document.getElementsByName('Box1')[0].value;
 	var box2 = document.getElementsByName('Box2')[0].value;
-	var out = "The button was pressed!"
+	var out = parseFloat(box1) * parseFloat(box2);
 	document.getElementsByName('output')[0].value= out;
 }
