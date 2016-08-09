a96670007856aa96ee037d94d695257bbe279673
Dom Farolino <domfarolino@gmail.com>
2016-08-09 13:03:10 -0400

    refactor(html+js): refactor some of the elements and JS since we are not using them inside a form element

--- codes.js
+++ codes.js
@@ -1,6 +1,6 @@
-function Button() {
-	var box1 = document.getElementsByName('Box1')[0].value;
-	var box2 = document.getElementsByName('Box2')[0].value;
-	var out = parseFloat(box1) * parseFloat(box2);
-	document.getElementsByName('output')[0].value= out;
+function add() {
+	var number1 = document.getElementById('Number1').value;
+  var number2 = document.getElementById('Number2').value;
+	var out = parseFloat(number1) * parseFloat(number2);
+	document.getElementById('output').innerText = out;
 }
