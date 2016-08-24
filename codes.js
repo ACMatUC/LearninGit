04eb0fff6417a1deb24b9d5ef9e6c99fa8b31c26
Kurt Lewis <kurt.jlewis15@gmail.com>
2016-08-24 19:09:53 -0400

    Refactor add() to button() and fixed formatting

--- codes.js
+++ codes.js
@@ -1,6 +1,6 @@
-function add() {
+function button() {
 	var number1 = document.getElementById('Number1').value;
-  var number2 = document.getElementById('Number2').value;
+  	var number2 = document.getElementById('Number2').value;
 	var out = parseFloat(number1) * parseFloat(number2);
 	document.getElementById('output').innerText = out;
 }
