08225a3be84c03d074f7a70271e7eb5b53246b7f
isiah_lloyd <robotdude101@gmail.com>
2016-08-25 18:59:39 -0400

    Spelling errors

--- codes.js
+++ codes.js
@@ -4,5 +4,5 @@ function button() {
 	var out = parseFloat(number1) * parseFloat(number2);
 	document.getElementById('output').innerText = out;
     var message = "Congrats on learning git at the ACM meeting";
-    document.getElementById("MessageBoc").innerText = message;
+    document.getElementById("MessageBox").innerText = message;
 }
