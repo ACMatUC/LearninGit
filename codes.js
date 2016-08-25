886167d30dc65a983e30c758a486f47249381609
Laura Tebben <ltebben09@gmail.com>
2016-08-25 18:59:34 -0400

    Add message

--- codes.js
+++ codes.js
@@ -3,4 +3,6 @@ function button() {
   	var number2 = document.getElementById('Number2').value;
 	var out = parseFloat(number1) * parseFloat(number2);
 	document.getElementById('output').innerText = out;
+	var message = "Congrats on learning Git at ACM";
+	document.getElementByID('MessageBox').innerText = Message;
 }
